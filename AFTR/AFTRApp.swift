e96c09751930195b4c755629014bd321765999d3
wilgotlucaci <wilgot.lucaci@iths.se>
2026-08-26 15:59:20 +0200

    Initial Commit

---
+++ AFTR/AFTRApp.swift
@@ -0,0 +1,17 @@
+//
+//  AFTRApp.swift
+//  AFTR
+//
+//  Created by Wilgot Lucaci on 2026-08-26.
+//
+
+import SwiftUI
+
+@main
+struct AFTRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
